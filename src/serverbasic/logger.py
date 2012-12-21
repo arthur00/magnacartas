514f2b0815df536b2287b939a61fd381a9d2d98d
gentimouton <gentimouton@gmail.com>
2012-12-21 15:31:57 -0800

    simpler server2

--- src/serverbasic/logger.py
+++
@@ -1,5 +0,0 @@
-import logging.config
-
-config_filepath = 'logging.conf'
-logging.config.fileConfig(config_filepath)
-logger = logging.getLogger('game')
--- src/serverpirate/main.py
+++
@@ -1,6 +0,0 @@
-from serverpirate.game import PirateGame
-
-# logging is automatically done through the logger called 'game'
-
-if __name__ == "__main__":
-    game = PirateGame(9000, '/game') # port, url
